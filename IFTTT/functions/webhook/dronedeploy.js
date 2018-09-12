50f8f57aeae1b612a8e4a96664be46de305b1d1a
Przemysław Duszyński <composer@ais.pl>
2018-09-12 16:28:12 +0200

    Used appropriate handler method

--- IFTTT/functions/webhook/dronedeploy.js
+++ IFTTT/functions/webhook/dronedeploy.js
@@ -3,4 +3,4 @@
 const bootstrap = require('@dronedeploy/function-wrapper');
 const handler = require('./handler');
 
-exports.dronedeploy = bootstrap((ctx) => (req, res) => handler.helloWorld(req, res, ctx));
+exports.dronedeploy = bootstrap((ctx) => (req, res) => handler.routeHandler(req, res, ctx));
